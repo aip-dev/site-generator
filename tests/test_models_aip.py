302994c56b429c89cbd42c54b2d1a7b41c9e2357
Luke Sneeringer <lukesneeringer@google.com>
2021-01-07 16:19:57 -0800

    feat: Add hotlinking for the `[free text][aip-###]` format. (#20)
    This makes Markdown links of the form `[some text here][aip-###]`
    hotlink correctly without having to explicitly define the `[aip-###]`
    component at the bottom of the file.

--- tests/test_models_aip.py
+++ tests/test_models_aip.py
@@ -33,6 +33,7 @@ def test_content_hotlinking(site):
     hunchback = site.aips[31]
     les_mis = site.aips[62]
     assert '[AIP-62](/62)' in hunchback.content
+    assert '[A Tale of Two Cities](/59)' in hunchback.content
     assert '[AIP-31](/31)' in les_mis.content
     assert '[[AIP-31](/31)](/31)' not in les_mis.content
 
